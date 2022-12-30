41ca679e122bc0945ab7d08325edef6dd1ac6152
singhtushar689 <singhtushar689@gmail.com>
2022-12-30 22:25:13 +0530

    Bubble Sort Method

---
+++ BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class BubbleSort
+    {
+        public void Sort()
+        {
+            int[] array = { 34, 73, 54, 23, 64, 86, 46 };
+            int temp;
+            for(int j = 0; j <= array.Length-2; j++)
+            {
+                for(int i = 0; i <= array.Length -2; i++)
+                {
+                    if (array[i] > array[i+1])
+                    {
+                        temp = array[i + 1];
+                        array[i+1] = array[i];
+                        array[i] = temp; 
+                    }
+                }
+            }
+            Console.WriteLine("Ascending order of the array is given");
+            foreach(int element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,11 +3,11 @@
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            //BubbleSort bubbleSort = new BubbleSort();
-            //bubbleSort.Sort();
-            InsertionSort sort = new InsertionSort();
-            sort.Sort();
+        {
+            Console.WriteLine("Hello World");
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.Sort();
+            
         }
     }
 }
