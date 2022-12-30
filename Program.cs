a1097553e045cdba22fd9f29834c653fb1e632eb
singhtushar689 <singhtushar689@gmail.com>
2022-12-30 22:22:56 +0530

    Hello World

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.Sort();
+            InsertionSort sort = new InsertionSort();
+            sort.Sort();
+        }
+    }
+}
